0240e23e387e52e6fcef1473ecf117588a73349d
RYusef <135138130+RYusef@users.noreply.github.com>
2024-01-02 14:25:26 +0600

    Create mario-more(2).c

---
+++ mario-more(2).c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Get Height of Bricks
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    // Limited input 1-8
+    while (n < 1 || n > 8);
+
+    // Print Pyramid with given Height
+    for (int i = 0; i < n; i++)
+    {
+        // Space = Height - Row - 1
+        for (int j = 0; j < n; j++)
+        {
+            if (j < n - i - 1)
+                printf(" ");
+            else
+                printf("#");
+        }
+        // Gap between two Pyramids
+        printf("  ");
+
+        // Second Pyramid of Same Height, Opposite Slant
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
